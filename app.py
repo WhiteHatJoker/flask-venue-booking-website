3b740b5ab660b6673ab3075e884aec6ed8be05e2
whitehatjoker <rkhudoyberdiev@gmail.com>
2020-04-13 23:25:16 +0500

    Add columns to Artist model

--- app.py
+++ app.py
@@ -59,13 +59,16 @@ class Venue(db.Model):
 
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(500))
 
 
 # TODO: implement any missing fields, as a database migration using Flask-Migrate
