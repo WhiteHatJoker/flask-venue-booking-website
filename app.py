c5f6f97d6e8a4ee1bf9400427500e11405db8ff7
whitehatjoker <rkhudoyberdiev@gmail.com>
2020-04-14 00:15:58 +0500

    Modified artists model

--- app.py
+++ app.py
@@ -49,12 +49,10 @@ class Venue(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
-    genres = db.Column(db.PickleType, nullable=False)
-    seeking_talent = db.Column(db.Boolean)
+    genres = db.Column(db.String(120), nullable=False)
+    seeking_talent = db.Column(db.Boolean, default=True)
     seeking_description = db.Column(db.String(500))
     artists = db.relationship('Artist', secondary=Show, backref=db.backref('venues', lazy=True))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 
 class Artist(db.Model):
@@ -67,13 +65,9 @@ class Artist(db.Model):
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
+    seeking_venue = db.Column(db.Boolean, default=True)
     seeking_description = db.Column(db.String(500))
 
-
-# TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 # ----------------------------------------------------------------------------#
 # Filters.
 # ----------------------------------------------------------------------------#
